928d652ae2869a36edae8cdf5d6e3df2a2fc9aba
Ryan Dooley <rdooley@conestogac.on.ca>
2025-03-19 11:18:44 -0400

    style comp for CreateSite

---
+++ resources/js/components/CreateSite.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import axios from 'axios';
+
+const createRepo = async () => {
+    try {
+       const response = await axios.post(route('create-repo'));
+        alert(response.data.response);
+        console.log(response.data)
+    } catch (e) {
+        console.log(e)
+        alert(e);
+    }
+};
+</script>
+
+<template>
+    <form @submit.prevent="createRepo">
+        <label class="block text-gray-700 dark:text-white font-medium mb-2">Band Name</label>
+        <input
+            type="text"
+            placeholder="Enter band name"
+            class="w-full p-2 border border-gray-300 rounded-lg"
+        />
+        <button
+            class="w-full mt-4 bg-gray-400 text-white py-2 rounded-lg font-semibold hover:bg-blue-700 dark:bg-blue-700 dark:hover:bg-gray-400">
+            Create Website
+        </button>
+    </form>
+</template>
--- resources/js/pages/Dashboard.vue
+++ resources/js/pages/Dashboard.vue
@@ -3,7 +3,7 @@ import AppLayout from '@/layouts/AppLayout.vue';
 import { type BreadcrumbItem } from '@/types';
 import { Head } from '@inertiajs/vue3';
 import PlaceholderPattern from '../components/PlaceholderPattern.vue';
-import MyComponent from '@/components/MyComponent.vue';
+import CreateSite from '@/components/CreateSite.vue';
 
 const breadcrumbs: BreadcrumbItem[] = [
     {
@@ -19,9 +19,13 @@ const breadcrumbs: BreadcrumbItem[] = [
     <AppLayout :breadcrumbs="breadcrumbs">
         <div class="flex h-full flex-1 flex-col gap-4 rounded-xl p-4">
             <div class="grid auto-rows-min gap-4 md:grid-cols-3">
-                <div class="relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border">
-                    <MyComponent />
+
+
+                <div class="flex items-center justify-center relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border">
+                    <CreateSite />
                 </div>
+
+
                 <div class="relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border">
                     <PlaceholderPattern />
                 </div>
